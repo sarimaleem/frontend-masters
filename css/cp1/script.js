80e1dbcdc292f43377c4707dccc4736e0dac89b9
Sarim Aleem <sarimaleem99@gmail.com>
2023-05-19 00:09:22 -0500

    initial

---
+++ css/cp1/script.js
@@ -0,0 +1,17 @@
+/* 
+Welcome to the Getting Started with CSS workshop!
+
+In this first exercise, complete the following:
+
+- Sign into your CodePen account or create one if needed.
+
+- Make your Codepen show the rendered design on the right and the code blocks on the left by going to Change View (to the right of the Settings button up top).
+
+- Fork this CodePen and save to your account:
+https://blog.codepen.io/documentation/forks/
+
+- Look at the text in the HTML block. Change the text to reflect your own life. Don't worry about marking this up with HTML just yet. Take out all of the [] brackets - they are there to show you where to change the text.
+
+- Don't forget to save!
+
+*/
